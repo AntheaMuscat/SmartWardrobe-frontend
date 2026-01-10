786499c54e37606a3fa19e51a9649a92bfefdbc7
Anthea Muscat <anthe.muscat06@gmail.com>
2026-01-10 18:45:07 +0100

    old version

--- src/index.js
+++ src/index.js
@@ -1,29 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import App from "./App";
 import Home from "./pages/Home";
 import Wardrobe from "./pages/Wardrobe";
 import AddClothes from "./pages/AddClothes";
-import Outfits from "./pages/Outfits";
+import Outfits from "./pages/Outfits"; 
 import CalendarPage from "./pages/CalendarPage";
 import Chatbot from "./pages/Chatbot";
 import Analytics from "./pages/Analytics";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
-    <App>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/wardrobe" element={<Wardrobe />} />
-        <Route path="/outfits" element={<Outfits />} />
-        <Route path="/add" element={<AddClothes />} />
-        <Route path="/calendar" element={<CalendarPage />} />
-        <Route path="/chatbot" element={<Chatbot />} />
-        <Route path="/analytics" element={<Analytics />} />
-      </Routes>
-    </App>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/wardrobe" element={<Wardrobe />} />
+      <Route path="/otifits" element={<Outfits />} />
+      <Route path="/add" element={<AddClothes />} />
+      <Route path="/calendar" element={<CalendarPage />} />
+      <Route path="/chatbot" element={<Chatbot />} />
+      <Route path="/analytics" element={<Analytics />} />
+    </Routes>
   </BrowserRouter>
 );
