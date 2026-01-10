045637065bf3aa55541829da6f9774297b9f711b
Anthea Muscat <anthe.muscat06@gmail.com>
2026-01-10 18:38:45 +0100

    fixing routes

--- src/App.js
+++ src/App.js
@@ -1,35 +1,18 @@
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import Home from "./pages/Home";
-import Wardrobe from "./pages/Wardrobe";
-import AddClothes from "./pages/AddClothes";
-import CalendarPage from "./pages/CalendarPage";
-import Chatbot from "./pages/Chatbot";
+import React from "react";
+import Navbar from "./components/Navbar";
 import "./App.css";
-import Analytics from "./pages/Analytics";
 
-function App() {
+function App({ children }) {
   return (
-    <Router>
-      <div className="app-container">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/wardrobe" element={<Wardrobe />} />
-          <Route path="/add" element={<AddClothes />} />
-          <Route path="/calendar" element={<CalendarPage />} />
-          <Route path="/chat" element={<Chatbot />} />
-          <Route path="/analytics" element={<Analytics />} />
-        </Routes>
+    <div className="app-container">
+      {/* Navbar is always visible */}
+      <Navbar />
 
-        <nav className="bottom-nav">
-          <Link to="/"></Link>
-          <Link to="/wardrobe"></Link>
-          <Link to="/add"></Link>
-          <Link to="/calendar"></Link>
-          <Link to="/chat"></Link>
-          <Link to="/analytics"></Link>
-        </nav>
+      {/* Page content will be rendered here via Routes in index.js */}
+      <div className="page-content">
+        {children}
       </div>
-    </Router>
+    </div>
   );
 }
 
